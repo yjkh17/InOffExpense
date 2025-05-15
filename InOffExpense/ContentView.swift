f8a72adaeca07c92caddf6bc72d7884a76c39548
yjkh17 <yj.kh17@gmail.com>
2025-05-15 04:24:23 +0300

    Initial Commit

---
+++ InOffExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InOffExpense
+//
+//  Created by Yousef Jawdat on 15/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InOffExpense/InOffExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  InOffExpenseApp.swift
+//  InOffExpense
+//
+//  Created by Yousef Jawdat on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct InOffExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
